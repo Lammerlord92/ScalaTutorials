ed025209132be0356470eb035bc19f2c833efba4
JavCarFer <javcarfer92@gmail.com>
2018-12-19 00:18:16 +0100

    Funciones 1

--- src/lectures/part1basics/Expressions.scala
+++ src/lectures/part1basics/Expressions.scala
@@ -14,5 +14,34 @@ object Expressions extends App {
   aVariable/=5
   println(aVariable)
 
-  //Instrucciones (Haz) vs Expresiones (Valores)
+  //Instrucciones (Haz) vs Expresiones (Dame valor de)
+  val aCondition=true
+  val aConditionedValue=if(aCondition) 5 else 3 //IF EXPRESSION
+  println (aConditionedValue)
+
+  //NUNCA ESCRIBAS ESTO EN SCALA, EVITA BUCLES
+  var i=0
+  while(i<10){
+    println(i)
+    i+=1
+  }
+  //LOS BUCLES SON DE UNA PROGRAMACIÓN IMPERATIVA, CLÁSICA DE JAVA
+  //TODO EN SCALA ES UNA EXPRESIÓN MENOS VARIABLES Y PAQUETES
+
+  val aWeirdValue =(aVariable=3) //Tipo Unit===void (Lo que devuelve no es significativo)
+  println(aWeirdValue) //Devuelve un objeto vacio
+
+  //sideEffects -> println,whiles, reasignación de variables, debería declararse así:
+  i=0
+  val aWhile=  while(i<10){  //aWhile es de tipo Unit
+    println(i)
+    i+=1
+  }
+
+  //Code blocks
+  val aCodeBlock={ //Expresión
+    val y=2
+    val z=y+1
+    if (z>2) "hello" else "goodbye"
+  }
 }
